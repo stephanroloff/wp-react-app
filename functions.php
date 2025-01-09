f8ae86b047d6880a8a71b85a17a95046dfc24333
Stephan Roloff <sr@werbeagenten.de>
2025-01-09 11:19:13 +0100

    adding auth_wp_routing again

--- functions.php
+++ functions.php
@@ -31,6 +31,22 @@ function custom_auth_shortcode_react() {
 }
 add_shortcode('react_app', 'custom_auth_shortcode_react');
 
+//Shortcode to use the WP Routing
+//It has to be used just with HashRoute
+function custom_auth_shortcode_wp() {
+    if ( !is_user_logged_in() ) {
+        wp_safe_redirect( wp_login_url() );
+        exit;
+    } else {
+        $page_id = get_the_ID();
+        $page = get_post($page_id);
+        if ($page) {
+            echo apply_filters('the_content', $page->post_content);
+        }
+    }
+}
+add_shortcode('auth_wp_routing', 'custom_auth_shortcode_wp');
+
 //Enqueue Styles
 function my_theme_enqueue_styles() {
     wp_enqueue_style(
